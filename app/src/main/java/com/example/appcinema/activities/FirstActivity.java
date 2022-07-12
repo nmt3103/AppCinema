2a21f0f6afc521be11af3a610d8ee42f745e2799
Nguyen Tien <tien31032000@gmail.com>
2022-07-12 10:02:22 +0700

    7/12/2022 day start for login register shareference class for user

---
+++ app/src/main/java/com/example/appcinema/activities/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.example.appcinema.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appcinema.R;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
