57670ab9abd7bb371f39619d22e624446f06f563
Nguyen Tien <tien31032000@gmail.com>
2022-07-12 11:10:06 +0700

    7/12/2022 day update login gape

--- app/src/main/java/com/example/appcinema/MainActivity.java
+++ app/src/main/java/com/example/appcinema/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.appcinema;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.appcinema.activities.RegisterActivity;
+
+import com.google.android.material.button.MaterialButton;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText edEmail,edPassword;
+    MaterialButton btnSignIn;
+    ProgressBar progressBar;
+    TextView tvNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edEmail = findViewById(R.id.inputEmail);
+        edPassword = findViewById(R.id.inputPassword);
+        btnSignIn = findViewById(R.id.buttonSignIn);
+        progressBar = findViewById(R.id.progressBar);
+        tvNew = findViewById(R.id.textCreateNewAccount);
+
+
+
+
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        tvNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValidSignInDetails()){
+                    signIn();
+                }
+            }
+        });
+    }
+
+    private void signIn() {
+        loading(true);
+
+    }
+
+    private boolean isValidSignInDetails() {
+        if (edEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(edEmail.getText().toString()).matches()){
+            showToast("Enter valid email");
+            return false;
+        } else if (edPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void loading(Boolean isLoading){
+        if (isLoading){
+            btnSignIn.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            btnSignIn.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/appcinema/model/User.java
@@ -0,0 +1,7 @@
+package com.example.appcinema.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name,image,email,token,id;
+}
