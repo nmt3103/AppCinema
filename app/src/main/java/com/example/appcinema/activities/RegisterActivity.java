5db2c9dd15d74260818586d619bb1f8bd7bfdcbe
Nguyen Tien <tien31032000@gmail.com>
2022-07-12 08:23:46 +0700

    7/12/2022 day start for login register add library

---
+++ app/src/main/java/com/example/appcinema/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.appcinema.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appcinema.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
